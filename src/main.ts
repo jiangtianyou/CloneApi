e6a0be184656fa37ea262612313c5589d7cce407
jiangtianyou <420671626@qq.com>
2019-11-25 14:00:46 +0800

    change

--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,34 @@ import { saveCollection } from './scripts/postman';
 import { createConfig, initEnv } from './scripts/setEnv';
 import * as program from 'commander';
 import * as ck from 'chalk';
+import * as ora from 'ora';
 
 const exit = process.exit,
   log = console.log;
 
-program.version('1.0.0')
-  .arguments('<bean> [key]')
-  .description('clone小幺鸡api到postman')
-  .action(async function(bean, key) {
-    if (bean === 'config' && key && key.length > 5) {
-      createConfig({ key: key });
-      exit();
-    }
-    initEnv();
-    let result: ApiItem[] = await parse(bean);
-    if (result.length === 0) {
-      log(ck.bgRedBright('未提取到有效的api数据，程序将要推出'));
-      exit();
-    }
-    await saveCollection(result);
-  })
-  .parse(process.argv);
+// program.version('1.0.0')
+//   .arguments('<bean> [key]')
+//   .description('clone小幺鸡api到postman')
+//   .action(async function(bean, key) {
+//     if (bean === 'config' && key && key.length > 5) {
+//       createConfig({ key: key });
+//       exit();
+//     }
+//     initEnv();
+//     let result: ApiItem[] = await parse(bean);
+//     if (result.length === 0) {
+//       log(ck.bgRedBright('未提取到有效的api数据，程序将要推出'));
+//       exit();
+//     }
+//     await saveCollection(result);
+//   })
+//   .parse(process.argv);
 
 
+
+const spinner = ora('Loading unicorns').start();
+
+setTimeout(() => {
+  spinner.color = 'yellow';
+  spinner.text = 'Loading rainbows';
+}, 1000);
