2b1c9862931f5417bee824f14064f7f00224f5ee
jiangtianyou <420671626@qq.com>
2019-11-25 18:15:51 +0800

    change

--- src/utils/RequestUtil.ts
+++ src/utils/RequestUtil.ts
@@ -3,10 +3,10 @@ import * as ora from 'ora';
 import * as chalk from 'chalk';
 
 export async function getUrlContent(path: string): Promise<string> {
-  const downloadSpinner = ora('正在下载页面'+chalk.bgGreen(path)+'的数据').start()
+  const downloadSpinner = ora(`正在下载页面${path}的数据`).start()
   let requestPromise = req.get(path);
   return requestPromise.then((res) => {
-    downloadSpinner.succeed(`下载页面${chalk.bgGreen(path)}成功`);
+    downloadSpinner.succeed(`下载页面${path}成功`);
     return res;
   }).catch(err => {
     downloadSpinner.fail('下载失败');
